9b84ba778b2557d4def539539fb7ef7ceb0ac989
Ernest Chidakwa <149294108+E40sr@users.noreply.github.com>
2024-08-10 21:36:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let pics = document.querySelectorAll('.pic');
 
+  pics.forEach(function (pic, index) {
+    let row = Math.floor(index / 3);
+
+    let column = index % 3;
+
+    let left = (column / 2) * 100 + "%";
+
+    let top = ( row / 2) * 100 + "%";
+
+    pic.style.transformOrigin = left + " " + top;
+
+    pic.addEventListener( function(){
+      pics.addEventListener( function(p){
+        p.classList.toggle('away')
+      })
+      pic.classList.remove('away')
+      pic.classList.toggle('active')
+    })
+  })
+})
