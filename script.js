71d0cb0b0de8f3f403998f6f9b4f087370a9700b
Ernest Chidakwa <149294108+E40sr@users.noreply.github.com>
2024-08-10 22:19:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     pic.addEventListener('click', function(){
       pics.forEach( function(p){
-        p.classList.toggle('away')
-        p.classList.remove('active')
+        p.classList.toggle('away');
       })
       pic.classList.remove('away')
       pic.classList.toggle('active')
